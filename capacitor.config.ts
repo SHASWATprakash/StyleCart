00d790606747962bb500701f905ec21c8a28fae2
Shaswat Prakash <shaswat.p1692@gmail.com>
2025-04-16 13:26:33 +0000

    "Add it on the git "

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.styleCartNewE-commerce',
+  appName: 'StyleCart',
+  webDir: 'out',
+ 
+};
+
+export default config;
